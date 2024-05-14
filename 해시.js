fbbd2390784e8d26d9f29a31a12f30184d620f0b
aazkgh <gg25hp17@naver.com>
2024-05-14 15:03:32 +0900

    hash 문풀 수정

--- 해시.js
+++ 해시.js
@@ -22,70 +22,36 @@ function solution(participant, completion) {
 
 //1v2.전화번호 목록
 function solution(phone_book) {
-  const hash = new Map();
+  let answer = true;
   
-  phone_book.forEach(number => {
-      hash[number] = true;
-  });
-  
-  for (let i = 0; i < phone_book.length; i++) {
-      for (let j = 1; j < phone_book[i].length; j++) {
-          const prefix = phone_book[i].substring(0, j);
-          if (hash[prefix]) {
-              return false;
-          }
+  phone_book.sort();
+  for (let i =0; i<phone_book.length-1; i ++){
+      if (phone_book[i+1].startsWith(phone_book[i])){
+          answer = false;
+          break;
       }
   }
   
-  return true;
+  return answer;
 }
 
 //1v2.의상
 function solution(clothes) {
   const hash = new Map();
   
-  clothes.forEach(([_, type]) => {
-    hash[type] = (hash[type] || 0) + 1;
-  });
+  for(let i = 0; i< clothes.length; i++){
+      if(hash.has(clothes[i][1])){
+          const currentVal = hash.get(clothes[i][1]);
+          hash.set(clothes[i][1], currentVal+1);
+      } else {
+          hash.set(clothes[i][1], 1);
+      }
+  }
   
   let answer = 1;
-  for (let key in hash) {
-      answer *= (hash[key] + 1);
+  for (const num of hash.values()) {
+      answer *= num+1;
   }
-
+  
   return answer - 1;
 }
-
-//lv3.베스트앨범 - 솔루션 참고
-function solution(genres, plays) {
-    const genreCountMap = new Map();
-    const albums = genres.map((genre, idx) => ({
-      genre,
-      plays: plays[idx],
-      idx,
-    }));
-  
-    albums.forEach(({ genre, plays }) => {
-      genreCountMap.set(genre, (genreCountMap.get(genre) || 0) + plays);
-    });
-  
-    const sortedAlbums = albums.sort((a, b) => {
-      if (a.genre !== b.genre)
-        return genreCountMap.get(b.genre) - genreCountMap.get(a.genre);
-      if (a.plays !== b.plays) return b.plays - a.plays;
-      return a.idx - b.idx;
-    });
-  
-    const selectedAlbums = new Map();
-    const answer = [];
-  
-    sortedAlbums.forEach(({ genre, idx }) => {
-      const albumCount = selectedAlbums.get(genre) || 0;
-      if (albumCount < 2) {
-        selectedAlbums.set(genre, albumCount + 1);
-        answer.push(idx);
-      }
-    });
-  
-    return answer;
-  }
