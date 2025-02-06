1745a68186a326f9d8f568c3c0f5aeeb16414f93
고가형 <101045330+aazkgh@users.noreply.github.com>
2025-02-07 00:47:41 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 160 ms, Memory: 9672 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.js
@@ -1,33 +1,16 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-let input = [];
-rl.on('line', function (line) {
-  input.push(line)
-})
-  .on('close', async function () {
-  // 답안 작성
-  let answer = '';
-  let n = input[0];
-  let arr = '1001 '+ input[1]
-  arr = arr.split(' ')
-  answer = max_decrease_array(n, arr);  
-  console.log(answer)
-  process.exit();
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-function max_decrease_array(n, arr){
-  let dp = new Array(parseInt(n)+1).fill(0)
-  for(let i=1;i<=n;i++){
-    let max = 0;
-    for(let j=0;j<i;j++){
-      if(arr[j]*1 > arr[i]*1){
-        max = Math.max(dp[j],max)
-      }
+const N = parseInt(input[0]);
+const arr = input[1].split(' ').map(Number);
+const dp = new Array(N).fill(1);
+
+for (let i = 1; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[i] < arr[j]) {
+            dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
     }
-    dp[i] = max+1;
-  }
-  return Math.max(...dp)
 }
+
+console.log(Math.max(...dp));
