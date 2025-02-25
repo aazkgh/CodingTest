d13f18da04fa32e347160418e0f1cd19cf57981e
고가형 <101045330+aazkgh@users.noreply.github.com>
2025-02-26 00:17:10 +0900

    [Silver I] Title: Milk Factory, Time: 152 ms, Memory: 11020 KB -BaekjoonHub

---
+++ 백준/Silver/17199. Milk Factory/Milk Factory.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+let containers = Array.from({ length: N+1 }, () => []);
+
+for(let i=1; i<input.length; i++){
+    let [start, end] = input[i].split(' ').map(Number);
+    containers[start].push(end);
+}
+
+let answer = [];
+
+function track(v, visited, target){
+    if(visited[v]) return false;
+    if(v === target) return true;
+
+    visited[v] = true;
+     for(let next of containers[v]) {
+        if(track(next, visited, target)) return true;
+    }
+    
+    return false;
+}
+
+
+for (let i = 1; i <= N; i++) {
+    let canReach = true;
+
+    for (let j = 1; j <= N; j++) {
+        if (j !== i) {
+            let visited = new Array(N + 1).fill(false);
+            if (!track(j, visited, i)) {
+                canReach = false;
+                break;
+            }
+        }
+    }
+
+    if(canReach) answer.push(i);
+}
+
+if(answer.length){
+    console.log(Math.min(...answer));  
+} else{
+    console.log(-1);
+}
