2fba19465a76de7e38e64c83340e9d27c582dc05
고가형 <101045330+aazkgh@users.noreply.github.com>
2025-08-27 21:22:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 156 ms, Memory: 9652 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,17 @@
+const N = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
+const MOD = 10007
+
+if(N === 1) {
+    console.log(1);
+    return;
+}
+
+const dp = new Array(N + 1).fill(0);
+dp[1] = 1;
+dp[2] = 2;
+
+for(let i = 3; i <= N; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD ;
+}
+
+console.log(dp[N]);
